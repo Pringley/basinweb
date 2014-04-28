1d1da5504038280f09299f562f401f39b83dc1a6
Ben Pringle <ben.pringle@gmail.com>
2014-04-28 00:39:55 -0400

    Set up initial object model

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,48 @@
 from django.contrib import admin
+from tasks.models import Task, Label, Assignee
 
-# Register your models here.
+class LabelInline(admin.StackedInline):
+    model = Label
+    extra = 1
+
+class TaskAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {
+            'fields': ['summary', 'completed', 'due', 'project', 'labels', 'details'],
+        }),
+        ('Internal', {
+            'fields': ['sleepuntil', 'sleepforever', 'assignee', 'blockers', 'trashed'],
+        }),
+        ('Timestamp', {
+            'fields': ['created', 'modified'],
+        }),
+    ]
+    filter_horizontal = ['labels', 'blockers']
+    readonly_fields = ('created', 'modified')
+    save_on_top = True
+
+    list_display = ('summary', 'due', 'completed', 'is_active', 'is_sleeping',
+            'is_blocked', 'is_delegated')
+    list_editable = ('completed',)
+    list_filter = ('due', 'created', 'completed', 'trashed')
+    search_fields = ('summary', 'assignee', 'details')
+
+class LabelAdmin(admin.ModelAdmin):
+    fields = ('name', 'trashed')
+    save_on_top = True
+
+    list_display = ('name', 'trashed')
+    list_search = ('name',)
+    list_filter = ('trashed',)
+
+class AssigneeAdmin(admin.ModelAdmin):
+    fields = ('name', 'last_request', 'last_response', 'trashed')
+    save_on_top = True
+
+    list_search = ('name',)
+    list_display = ('name', 'last_request', 'last_response', 'trashed')
+    list_filter = ('last_request', 'last_response', 'trashed')
+
+admin.site.register(Task, TaskAdmin)
+admin.site.register(Label, LabelAdmin)
+admin.site.register(Assignee, AssigneeAdmin)
--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,92 @@
 from django.db import models
+from django.utils.timezone import now
 
-# Create your models here.
+class Label(models.Model):
+    """A task label, such as 'school' or 'work'."""
+    name = models.CharField(max_length=50)
+    trashed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+class Assignee(models.Model):
+    """Someone to whom you can delegate tasks."""
+    name = models.CharField(max_length=100)
+
+    last_request = models.DateTimeField(null=True, blank=True,
+            help_text='Last time you asked this person for something.')
+    last_response = models.DateTimeField(null=True, blank=True,
+            help_text='Last time this person responded to you.')
+
+    trashed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+class Task(models.Model):
+    """A todo item, such as 'take out trash'."""
+    summary = models.CharField(max_length=144, null=True, blank=True)
+    details = models.TextField(null=True, blank=True)
+    due = models.DateTimeField(null=True, blank=True)
+    completed = models.BooleanField(default=False)
+    labels = models.ManyToManyField(Label, related_name='tasks', blank=True)
+
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    # Allow tasks to be marked as high-level projects.
+    project = models.BooleanField(default=False,
+            help_text="Set to True if this is a high-level project "
+                      "(as opposed to a granular action).")
+
+    # Instead of deleting tasks, simply mark them as "trashed".
+    trashed = models.BooleanField(default=False,
+            help_text="Instead of deleting tasks, simply mark them "
+                      "as 'trashed' here.") 
+
+    # Deferred tasks are "sleeping". The "sleepuntil" field tells the task when
+    # to wake back up. To defer a task indefinitely, set sleepforever=True.
+    sleepuntil = models.DateTimeField("sleep until", null=True, blank=True,
+            help_text="Put this task to sleep until specified time.")
+    sleepforever = models.BooleanField("sleep indefinitely", default=False,
+            help_text="Put this task to sleep indefinitely.")
+
+    # Tasks can be "blocked" on other tasks. Blocked tasks have a non-empty set
+    # of "blockers".
+    blockers = models.ManyToManyField('self', related_name='blocking',
+            symmetrical=False, blank=True,
+            help_text="Select any other tasks preventing this from completion.")
+
+    # Delegated tasks have a non-null "assignee" reference.
+    assignee = models.ForeignKey(Assignee, null=True, blank=True,
+            related_name='tasks',
+            help_text="Delegate tasks by specifying an assignee.")
+
+    def is_active(self):
+        """Check if the task is active (non-sleeping, etc)."""
+        return not (self.completed or self.trashed or self.is_delegated()
+                or self.is_sleeping() or self.is_blocked())
+    is_active.boolean = True
+    is_active.short_description = 'Active?'
+
+    def is_sleeping(self):
+        """Check if the task is sleeping/deferred."""
+        return self.sleepforever or (self.sleepuntil is not None
+                and now() < self.sleepuntil)
+    is_sleeping.boolean = True
+    is_sleeping.short_description = 'Sleeping?'
+
+    def is_blocked(self):
+        """Check if the task is blocked/waiting."""
+        return bool(self.blockers.filter(completed=False))
+    is_blocked.boolean = True
+    is_blocked.short_description = 'Blocked?'
+
+    def is_delegated(self):
+        """Check if the task is assigned to someone else."""
+        return self.assignee is not None
+    is_delegated.boolean = True
+    is_delegated.short_description = 'Delegated?'
+
+    def __str__(self):
+        return self.summary
