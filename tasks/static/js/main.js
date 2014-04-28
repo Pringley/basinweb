e16b9a2f7aa3e4437c87e05f895a74cbba026112
Ben Pringle <ben.pringle@gmail.com>
2014-04-28 18:47:46 -0400

    Simplify Backbone views

--- tasks/static/js/main.js
+++ tasks/static/js/main.js
@@ -1,32 +1,45 @@
+var list;
 $(function() {
+    'use strict';
+
     var Task = Backbone.Model.extend({});
+    var TaskList = Backbone.Collection.extend({ model: Task })
 
-    var TaskList = Backbone.Collection.extend({
-        model: Task,
-        url: '/api/tasks/'
-    });
-
-    var TaskView = Backbone.View.extend({
+    var ItemView = Backbone.View.extend({
         tagName: 'li',
-        template: _.template($('#task-template').html()),
+        template: _.template($('#item-template').html()),
+        initialize: function() {
+            this.render();
+        },
         render: function() {
             this.$el.html(this.template(this.model.toJSON()));
-            return this;
         }
     });
 
-    var AppView = Backbone.View.extend({
-        el: $('#task-app'),
+    var ListView = Backbone.View.extend({
+        tagName: 'ul',
         initialize: function() {
-            this.tasks = new TaskList();
-            this.listenTo(this.tasks, 'add', this.addOne);
-            this.tasks.fetch();
+            this.listenTo(this.model, 'all', this.render);
         },
-        addOne: function(task) {
-            var view = new TaskView({model: task});
-            this.$('#task-list').append(view.render().el);
+        addItem: function(item) {
+            var view = new ItemView({model: item});
+            this.$el.append(view.el);
+        },
+        render: function() {
+            this.$el.html('');
+            this.model.each(this.addItem, this);
         }
     });
 
-    var App = new AppView();
+    var listViewFromUrl = function(url) {
+        var list = new TaskList();
+        list.url = url;
+        var view = new ListView({model: list});
+        list.fetch({reset: true});
+        return view;
+    };
+
+    var view = listViewFromUrl('/api/tasks/')
+    $('#main-task-list').append(view.el);
+
 });
