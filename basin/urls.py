edc3a902a64c364168df6c10ebb825bd9e65a974
Ben Pringle <ben.pringle@gmail.com>
2014-04-29 10:54:57 -0400

    Move static mockup to root

--- basin/urls.py
+++ basin/urls.py
@@ -6,8 +6,8 @@ from basin.routers import api_router
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', 'basin.views.index'),
-    url(r'^display/', 'basin.views.display'),
+    url(r'^$', 'basin.views.display'),
+    url(r'^backbone/', 'basin.views.index'),
     url(r'^api/', include(api_router.urls)),
     url(r'^admin/', include(admin.site.urls)),
 )
