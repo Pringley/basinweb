15e4e0e44580a5fbaf7ecb6f6d8107a1881f69d2
Ben Pringle <ben.pringle@gmail.com>
2014-05-13 01:30:21 -0400

    Reorganize js code

--- basin/static/js/alt.js
+++ basin/static/js/alt.js
@@ -1,6 +1,6 @@
 var TASKS_URL = "/api/tasks/";
 
-var state = 'active';
+var global_state = 'active';
 var states = ['active', 'sleeping', 'blocked', 'delegated', 'completed', 'trashed'];
 
 function update(task, fields) {
@@ -17,71 +17,70 @@ function update_state(task, element, fields) {
     });
 }
 
-function refresh(new_state) {
+function render_task(task, state) {
+    var item = $('<li>').addClass('task');
+    var contents = _.template($('#task-template').html(), {
+        task: task
+    });
+    item.html(contents);
+    item.find('.complete-btn')
+    .click(function() {
+        update_state(task, item, { completed: !task.completed });
+    });
+    item.find('.trash-btn')
+    .click(function() {
+        update_state(task, item, { trashed: !task.trashed });
+    });
+    item.find('.sleep-btn')
+    .click(function() {
+        if (state !== 'sleeping') {
+            var sleepuntil = prompt("Sleep until:", (new Date()).toISOString());
+            update_state(task, item, { sleepuntil: sleepuntil });
+        }
+        else {
+            update_state(task, item, { sleepuntil: null, sleepforever: false });
+        }
+    });
+    item.find('.delegate-btn')
+    .click(function() {
+        if (state !== 'delegated') {
+            var delegatedto = prompt("Delegate to:");
+            update_state(task, item, { delegatedto: delegatedto });
+        }
+        else {
+            update_state(task, item, { delegatedto: '' });
+        }
+    });
+    if (state === 'completed') {
+        item.find('.sleep-btn').hide();
+        item.find('.delegate-btn').hide();
+        item.find('.complete-btn').html('Un-complete')
+    }
+    if (state === 'trashed') {
+        item.find('.complete-btn').hide();
+        item.find('.sleep-btn').hide();
+        item.find('.delegate-btn').hide();
+        item.find('.trash-btn').html('Un-trash')
+    }
+    if (state === 'delegated') {
+        item.find('.delegate-btn').html('Un-delegate');
+    }
+    if (state === 'sleeping') {
+        item.find('.sleep-btn').html('Un-sleep');
+    }
+    return item;
+}
+
+function refresh(state) {
     var mainlist = $('.mainlist');
-    mainlist.html('');
     $.ajax({
         url: "/api/tasks/"
     }).done(function(tasks) {
-        state = new_state;
+        mainlist.html('');
+        global_state = state;
         _.each(tasks, function(task) {
             if (filters[state](task)) {
-                var item = $('<li>').addClass('task');
-                var contents = _.template($('#task-template').html(), {
-                    task: task
-                });
-                item.html(contents);
-                item.find('.complete-btn')
-                .click(function() {
-                    update_state(task, item, { completed: !task.completed });
-                });
-                item.find('.trash-btn')
-                .click(function() {
-                    update_state(task, item, { trashed: !task.trashed });
-                });
-                item.find('.sleep-btn')
-                .click(function() {
-                    var sleeping = filters['sleeping'](task);
-                    var sleepuntil;
-                    if (!sleeping) {
-                        sleepuntil = prompt("Sleep until:", (new Date()).toISOString());
-                    }
-                    update_state(task, item,
-                        !filters['sleeping'](task)
-                            ? { sleepuntil: sleepuntil }
-                            : { sleepuntil: null, sleepforever: false }
-                    );
-                });
-                item.find('.delegate-btn')
-                .click(function() {
-                    var delegatedto;
-                    if (task.delegatedto === '') {
-                        delegatedto = prompt("Delegate to:");
-                    }
-                    update_state(task, item, {
-                        delegatedto:
-                            task.delegatedto === ''
-                                ? delegatedto
-                                : ''
-                    });
-                });
-                if (state === 'completed') {
-                    item.find('.sleep-btn').hide();
-                    item.find('.delegate-btn').hide();
-                    item.find('.complete-btn').html('Un-complete')
-                }
-                if (state === 'trashed') {
-                    item.find('.complete-btn').hide();
-                    item.find('.sleep-btn').hide();
-                    item.find('.delegate-btn').hide();
-                    item.find('.trash-btn').html('Un-trash')
-                }
-                if (state === 'delegated') {
-                    item.find('.delegate-btn').html('Un-delegate');
-                }
-                if (state === 'sleeping') {
-                    item.find('.sleep-btn').html('Un-sleep');
-                }
+                var item = render_task(task, state);
                 mainlist.append(item);
             }
         });
@@ -129,7 +128,7 @@ $(function() {
 
     _.each(states, function(btnstate) {
         var button = $('.nav .' + btnstate);
-        highlightButton(state);
+        highlightButton(global_state);
         button.click(function() {
             refresh(btnstate);
             highlightButton(btnstate);
